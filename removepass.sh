c8e5fb62d7ff0f3143038661f43b2071c3d80fc3
VladTop46 <79125199+VladTop46@users.noreply.github.com>
2022-10-21 20:33:21 +0900

    Tested version, work on Windows 10

--- removepass.sh
+++ removepass.sh
@@ -58,15 +58,15 @@ sudo cp /mnt/windows/Windows/System32/config/SAM /mnt/windows/Windows/System32/c
 sudo cp /mnt/windows/Windows/System32/config/SAM /mnt/windows/Windows/SAM.bak
 sleep 1
 
+/usr/sbin/chntpw /mnt/windows/Windows/System32/config/SAM -l
 echo 'All users on this PC, please select one:'
-/usr/sbin/chntpw /mnt/Windows/System32/config/SAM -l
 read u
 echo 'User' $u 'is selected, continue? [Y/N]'
 read c
 
 case $c in 
 y)
-/usr/sbin/chntpw /mnt/Windows/System32/config/SAM -u $u
+/usr/sbin/chntpw /mnt/windows/Windows/System32/config/SAM -u $u
 sleep 1
 echo 'Successfully deleted password for' $u
 exit 0
